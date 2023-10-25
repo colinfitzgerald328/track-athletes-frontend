db447a3c56a6cc1746cb2501c89bc74072b1caf9
Colin <colinfitzgerald328@gmail.com>
2023-10-25 00:27:19 -0700

    add latest

--- src/app/AthleteCard.js
+++ src/app/AthleteCard.js
@@ -1,31 +1,37 @@
-// AthleteCard.js
-
 import React from 'react';
+import styles from './styles.module.css';
+import Link from 'next/link';
 
 const AthleteCard = ({ athlete }) => {
+  const { full_name, hq_image_url, wikipedia_url, instagram_url, summary } = athlete;
+
   return (
-    <div className="athlete-card">
-      <img src={athlete.image_url} alt={`${athlete.givenName} ${athlete.familyName}`} />
-      <h2>{`${athlete.givenName} ${athlete.familyName}`}</h2>
-      <p>Birth Date: {athlete.birthDate}</p>
-      <p>Disciplines: {athlete.disciplines}</p>
-      <p>Country: {athlete.country}</p>
-      {athlete.wikipedia_url && (
-        <p>
-          <a href={athlete.wikipedia_url} target="_blank" rel="noopener noreferrer">
-            Wikipedia
+    <div className={styles.card}>
+      <div className={styles.cardHeader}>
+        <h1>{full_name}</h1>
+        <div className={styles.socials}>
+        <Link legacyBehavior href={wikipedia_url}>
+          <a>
+            <img className={styles.instagram} src="/wikipedia-logo.svg" alt="Wikipedia" />
           </a>
-        </p>
-      )}
-      {athlete.instagram_url && (
-        <p>
-          <a href={athlete.instagram_url} target="_blank" rel="noopener noreferrer">
-            Instagram
+        </Link>
+        <Link legacyBehavior href={instagram_url}>
+          <a>
+            <img className={styles.instagram} src="/instagram-logo.png" alt="Instagram" />
           </a>
-        </p>
-      )}
+        </Link>
+        </div>
+      </div>
+      <img className={styles.imageThing} src={hq_image_url} alt={full_name} />
+
+      <p className={styles.summary}>{summary}</p>
+
+
     </div>
   );
 };
+
+
+
 
 export default AthleteCard;
