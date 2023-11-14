03b58a8ad0b7ff870e6d1082ca3b6e8f22614813
Colin <colinfitzgerald328@gmail.com>
2023-11-14 10:04:41 -0800

    add score graphs

--- src/app/components/1_AthleteResults/index.js
+++ src/app/components/1_AthleteResults/index.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import Skeleton from "@mui/material/Skeleton";
-import DirectionsRunIcon from "@mui/icons-material/DirectionsRun";
+import GraphModal from "./1_GraphModal";
+import AutoGraphIcon from '@mui/icons-material/AutoGraph';
 import styles from "./styles.module.css";
 
 export default function AthleteResults(props) {
   const [athlete, setAthlete] = useState([]);
+  const [graphModalOpen, setGraphModalOpen] = useState(false);
   const [loadingNewAthlete, setLoadingNewAthlete] = useState(false);
   const { viewingAthlete } = props;
   const firstDiscipline = viewingAthlete.disciplines.split(",")[0].trim();
@@ -30,6 +32,15 @@ export default function AthleteResults(props) {
     return n % 2 == 0;
   }
 
+  function openGraphModal() {
+    setGraphModalOpen(true);
+  }
+
+  function closeGraphModal() {
+    setGraphModalOpen(false);
+  }
+
+
   const resultsMap = athlete.map((result, index) => {
     return (
       <div
@@ -47,7 +58,16 @@ export default function AthleteResults(props) {
   });
   return (
     <div className={styles.tableHolder}>
+      <GraphModal
+        athlete={athlete}
+        viewingAthlete={viewingAthlete}
+        graphModalOpen={graphModalOpen}
+        closeGraphModal={closeGraphModal}
+      >
+
+      </GraphModal>
       <div className={styles.tableHeader}>
+        <div className={styles.leftItemsHolder}>
         <img
           src={
             "https://cdn.icon-icons.com/icons2/2248/PNG/512/clock_fast_icon_137750.png"
@@ -55,6 +75,12 @@ export default function AthleteResults(props) {
           className={styles.imageIcon}
         />
         LATEST RESULTS
+        </div>
+        <div
+        onClick={() => openGraphModal()}
+        >
+          <AutoGraphIcon className={styles.graphIcon} />
+        </div>
       </div>
       <div className={styles.tableHeaderLabels}></div>
       <div className={styles.resultHolder}>
