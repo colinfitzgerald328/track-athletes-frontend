3cd756dc8a357369cebfcf94916fdf818525b20e
Colin <colinfitzgerald328@gmail.com>
2023-11-15 22:36:56 -0800

    style improvements

--- src/app/components/mobile/1_AthleteResults/1_GraphModal/index.js
+++ src/app/components/mobile/1_AthleteResults/1_GraphModal/index.js
@@ -20,7 +20,7 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: `calc(100% - 80px)`,
+  width: `calc(100% - 60px)`,
   height: 500,
   bgcolor: "#59252e",
   borderRadius: "30px",
